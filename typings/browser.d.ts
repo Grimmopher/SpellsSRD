215c7757232bd2909bad7a258f0ff4c692bc6db2
Grimm <christopher.grimm@velir.com>
2016-02-25 19:49:47 -0500

    removed directory

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser\ambient\express\express.d.ts" />
-/// <reference path="browser\ambient\lodash\lodash.d.ts" />
-/// <reference path="browser\ambient\node\node.d.ts" />
--- typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main\ambient\express\express.d.ts" />
-/// <reference path="main\ambient\lodash\lodash.d.ts" />
-/// <reference path="main\ambient\node\node.d.ts" />
