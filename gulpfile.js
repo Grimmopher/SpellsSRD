2b06ee30f23ac30e6b247258afd09e86508bfcd7
Tara Grimm <tararochegrimm@gmail.com>
2016-04-05 13:09:10 -0400

    tried to fix gulpfile. didn't. added table and header to index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+gulp.task('sass', function() {
+    return gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('./css'));
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('./sass/**/*.scss',['sass']);
 });
